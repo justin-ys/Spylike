dbecea8a14c235af0ebc8e387cec89a4810dc4ab
Justin <justinyoung106@gmail.com>
2025-01-12 23:25:01 -0500

    Add Spylike Tiles TSX

---
+++ game/Spylike Tiles.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Spylike Tiles" tilewidth="32" tileheight="32" tilecount="20" columns="10">
+ <editorsettings>
+  <export target="test.spm" format="Spylike Map"/>
+ </editorsettings>
+ <image source="tileset.png" width="321" height="65"/>
+ <tile id="0" type="Player"/>
+ <tile id="1" type="Skeleton"/>
+ <tile id="2" type="Lava"/>
+ <tile id="3" type="Spike"/>
+ <tile id="4" type="Wall"/>
+ <tile id="5" type="Goblin"/>
+ <tile id="6" type="Key"/>
+ <tile id="7" type="Door"/>
+ <tile id="8" type="Barbarian"/>
+ <tile id="9" type="Treasure"/>
+ <tile id="10" type="LevelTrans"/>
+ <tile id="11" type="Boss"/>
+</tileset>
